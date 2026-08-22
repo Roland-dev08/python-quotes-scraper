b0c864015348f0ebc3e308f045afd79b2192bf03
Roland-dev08 <roland.bajcsi@electromures.net>
2026-08-22 16:17:56 +0300

    Remove 01_Practice_Tasks from GitHub and move gitignore to root

--- 01_Practice_Tasks/12tizenkettedik.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-print("Kapcsolódás az internethez...")
-
-valasz = requests.get("https://example.com")
-
-print("\n--- A WEBOLDAL IGAZI FORRÁSKÓDJA (HTML) ---")
-print(valasz.text)
